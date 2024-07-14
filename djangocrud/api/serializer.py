7df0e7b37cd969944fdf34d2ac5f845eaec85b97
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2024-07-13 22:20:00 -0500

    Creando las URL y vistas para usar API Rest, navegando en root y api-auth

--- djangocrud/api/serializer.py
+++ djangocrud/api/serializer.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from models import Movie
+from .models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
